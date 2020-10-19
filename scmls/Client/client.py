cd5dfa95fc8d0a471b84c444caeef95b0c0aabd7
smmahe <smahendren37@gmail.com>
2020-10-19 15:58:00 +0800

    scmls_build

---
+++ scmls/Client/client.py
@@ -0,0 +1,63 @@
+from tornado.httpclient import AsyncHTTPClient, HTTPRequest
+from tornado.ioloop import IOLoop
+from tornado.escape import json_encode
+import json
+		
+class ClientV2:
+
+	def __init__(self):
+		self.session = AsyncHTTPClient()
+		self.timeout = 5
+		self.url = None
+		self.service = None
+		self.apiversion = 'v2'
+	
+	def initializeForService(self,prefix,uri,port,service):
+		self.url = prefix + "://"+ uri + ":" + str(port) +"/" + self.apiversion + "/actions"
+		print("setting url")
+		self.service = service
+
+	def seturl(self, url):
+		self.url = url
+
+	def geturl(self):
+		return self.url
+
+	def GetHTTPClient(self):
+		return self.session
+
+	def getApiVersion(self):
+		return self.apiversion
+
+	def makeRequest(self, httpmethod,op,pid, body=None,org='scnoop'):
+		headers = None
+		
+		if httpmethod == 'GET':
+			print(httpmethod)
+			body = None
+		else:
+			body = json.dumps(body)
+			headers = {"content-type":"application/json"}
+		
+		finalURI = self.geturl() + '?op='+ str(op) + '&org=' + str(org) + '&pid='+str(pid)
+		req = HTTPRequest(finalURI,method = httpmethod, body = body,request_timeout = self.timeout,headers=headers)
+		
+		async def toExecute():
+			try:
+				response = await self.session.fetch(req)
+				print(json.loads(response.body))
+			except Exception as e:
+				print("inside exception",str(e))
+				return e, None
+			return None, json.loads(response.body)
+		print("making request to ---", finalURI, "----")
+		io_loop = IOLoop.current()
+		io_loop.run_sync(toExecute)
+	
+	def __del__(self):
+		print("Deleting client")
+		self.session.close()
+
+def getClient():
+	client = ClientV2()
+	return client
---
+++ scmls/services/SCML/SCMLExperiments.py
@@ -0,0 +1,87 @@
+import Client.client as client
+import json
+import tornado.ioloop
+import os
+
+HOST = 'SCML_EXPERIMENTS_HOST'
+PROTOCOL = 'HTTP_PROTOCOL'
+PORT = 'PORT'
+
+class SCMLExperiments:
+    def __init__(self): 
+        self.scmlClient = client.getClient()
+        self.initialize()
+        
+
+    def initialize(self):
+        try:
+            if os.getenv(HOST):
+                uri = os.getenv(HOST)
+            else:
+                uri="127.0.0.1"
+                print("Host is not set")
+            if os.getenv(PROTOCOL):
+                prefix = os.getenv(PROTOCOL)
+            else:
+                prefix = 'http'
+                print("protocol env variable is not set")
+            if os.getenv(PORT):
+                print("port is not set")
+                port = os.getenv(PORT)
+            else:
+                port = 5000
+            self.scmlClient.initializeForService(prefix,uri,port,'SCMLExperiments')
+        except Exception:
+            raise Exception()
+        
+
+    def getExperiment(self,pid,expid):
+        json_data = {"expid":str(expid)}
+        res =  self.scmlClient.makeRequest(httpmethod='POST',op='scmlexperiments.getExperiment',pid=pid,body=json_data)
+        return res
+       
+
+    def listExperiments(self,pid):
+        res = self.scmlClient.makeRequest('GET',op='scmlexperiments.listExperiments',pid=pid) 
+        return res
+        
+
+    def getLatestRun(self,pid,expid):
+        json_data = {"expid":str(expid)}
+        res = self.scmlClient.makeRequest('POST','scmlexperiments.getLatestRun',pid=pid,body=json_data)
+        return res
+
+    def getRunForExperiment(self,pid,expid,run):
+        json_data = {"expid":str(expid),"run":str(run)}
+        res =  self.scmlClient.makeRequest('POST','scmlexperiments.getRunForExperiment',pid=pid,body=json_data)
+        return res
+
+    def getLastNRuns(self,pid,expid,N):
+        json_data = {"expid":str(expid),"N":str(N)}
+        res =  self.scmlClient.makeRequest('POST','scmlexperiments.getLastNRuns',pid=pid,body=json_data)       
+        return res
+    
+
+    def getQueryConfig(self,pid,expid):
+        obj = self.getExperiment(pid,expid)
+        return obj['config']['query']
+    
+    def getAlgorithmConfig(self,pid,expid):
+        obj = self.getExperiment(pid,expid)
+        return obj['config']['algorithm']
+
+    def getPredictionConfig(self,pid,expid):
+        obj = self.getExperiment(pid,expid)
+        return obj['config']['prediction']
+    
+    def getExperimentState(self,pid,expid):
+        obj = self.getExperiment(pid,expid)
+        return obj['State']
+
+    def getExperimentIds(self,pid):
+        res = self.getExperiments(pid)
+        ids = []
+        for item in res:
+            ids.append(item['ExperimentId'])
+        return ids
+
