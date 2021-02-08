370496aacb8b8d218520be214490b743effcbe6b
Shikha-SC <74171354+Shikha-SC@users.noreply.github.com>
2021-02-08 14:51:48 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ from setuptools import setup, find_packages
 
 setup(name='sc-tasks-python-sdk',
       version='0.0.1',
-      description='This package is for sctasks python SDK.',
+      description='This package is Python SDK for sctasks.',
       packages=find_packages(),
 )
