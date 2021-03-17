3e3a26d4a8d9888de636134b114f2359baece5c1
ShukujiNeel13 <neelshukuji@gmail.com>
2021-03-17 14:15:14 +0800

    Pretty print the test response.

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
-from SDK.SCGridsServices.API import * 
+from pprint import pformat
+from SDK.SCGridsServices.API import *
 from SDK.SCDashboardServices.API import *
 from SDK.SCMetricsServices.API import *
 from SDK.SCBIServices.API import *
@@ -13,13 +14,22 @@ bi = SCBi()
 org = 'SMARTCLEAN'
 PropId = '3b749a681d14446292b6c79b48403bbd'
 
-Property = grids.readProperty(org=org,pid = 'scnoop',propid = PropId,client='Sync')
-print(Property.json())
+Property = grids.readProperty(
+    org=org,
+    pid='scnoop',
+    propid=PropId,
+    client='Sync'
+)
+response_content = Property.json()
+print(pformat(response_content))
 # print(Property.status_code)
-
-
-
-
 
 # response = bi.getReportingServicesForPrincipalOrg(org='SMARTCLEAN',pid='scnoop',expJson = json.dumps(request_body), client='Sync')
 # print(response)
+
+
+def test_steps_in_readme():
+    ...
+
+# if __name__ == '__main':
+#     test_steps_in_readme()
