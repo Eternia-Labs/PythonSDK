d6c3a415dcd30f2e5c40b83a33bbfaa161c36a53
ShukujiNeel13 <neelshukuji@gmail.com>
2021-03-31 12:24:47 +0800

    Create test (sample grids function) complete.

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
+import os
 import json
 from pprint import pformat
 
 # from SDK.SCGridsServices.API import *
+# print('Warning: Everything imported from Grids service directory...')
 # from SDK.SCDashboardServices.API import *
 # from SDK.SCMetricsServices.API import *
 # from SDK.SCBIServices.API import *
@@ -10,6 +12,7 @@ from pprint import pformat
 # metrics = SCMetrics()
 # bi = SCBi()
 
+LOAD_ENV_VARS = True
 
 # region Default Data for tests
 TEST_ORG = 'SMARTCLEAN'
@@ -23,8 +26,7 @@ CLIENT_TYPE_SYNC = 'Sync'
 
 def test_grids_function(org: str = None, pid: str = None, local: bool = True, test_client=CLIENT_TYPE_ASYNC):
 
-    from SDK.SCGridsServices.API import SCGrids
-    grids = SCGrids()
+    print('Starting test for a function in Grids service..')
 
     if org is None:
         org = TEST_ORG
@@ -35,6 +37,11 @@ def test_grids_function(org: str = None, pid: str = None, local: bool = True, te
         prop_id = TEST_PROP_ID_LOCAL
     else:
         prop_id = TEST_PROP_ID_REMOTE
+
+    from SDK.SCGridsServices.API import SCGrids
+    print('SCGrids imported from respective service directory.')
+    grids = SCGrids()
+    print('SCGrids instantiated.')
 
     read_prop_resp = grids.readProperty(org, pid, prop_id, test_client)
 
@@ -67,5 +74,27 @@ def _test_get_reporting_services_for_principal():
     print(pformat(response_content))
 
 
-if __name__ == '__main':
+def load_env_vars(dotenv_filepath: str = None):
+
+    if dotenv_filepath is not None:
+        # print('Loading env variables from dotenv file')
+        # root_dir = os.path.dirname(os.path.abspath(__file__))
+        # filepath = f'{root_dir}/env-dev.env'
+        raise Exception('Currently not loading vars from dotenv file (TODO later)')
+
+    os.environ['USER_NAME_PYTHONSDK'] = 'scbot'
+    print('USER_NAME_PYTHONSDK set in environment.')
+    os.environ['PASSWORD_PYTHONSDK'] = 'dummy-password'
+    print('PASSWORD_PYTHONSDK set in environment.')
+
+
+def run_test():
+
+    if LOAD_ENV_VARS is True:
+        load_env_vars()
     test_grids_function()
+
+
+if __name__ == '__main__':
+
+    run_test()
