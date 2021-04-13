bb8195cbc1f5143e6f24e4c56d3f78bfe8e1198a
ShukujiNeel13 <neelshukuji@gmail.com>
2021-04-13 14:15:38 +0800

    Add functions to test 2 requests to SCGrids
    Following test functions added.
    1. Test read building
    2. Test read Zone
    
    Also added a generic signature for testing requests to SCGrids service
    (to be used in future when refactoring tests)

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+import json
 import os
 from pprint import pformat
 from tornado.httpclient import HTTPClientError
@@ -9,6 +10,12 @@ EXTRACT_DATA_FROM_SDK_RESPONSE = True
 
 CLIENT_TYPE_SYNC = 'Sync'
 CLIENT_TYPE_ASYNC = 'Async'
+
+
+# TODO: Added generic signature for grids request (to be used in future when refactoring tests)
+def test_grids_request(org: str, pid: str, **kwargs):
+    print('TODO generic grids request')
+    raise NotImplementedError
 
 
 def test_get_property_info(org: str = None, pid: str = None, prop_id: str = None, test_client=CLIENT_TYPE_ASYNC):
@@ -68,6 +75,122 @@ def test_get_property_info(org: str = None, pid: str = None, prop_id: str = None
         print(pformat(desired_data))
 
 
+# TODO: Check below function is compatible with test-python-sdk
+def test_get_building_info(org: str = None, pid: str = None, test_client=CLIENT_TYPE_ASYNC):
+
+    print('Starting test for get_building_info...')
+
+    if org is None:
+        org = os.environ['TEST_ORG']
+
+    if pid is None:
+        pid = os.environ['TEST_PID']
+
+    from SDK.SCGridsServices.API import SCGrids
+    print('SCGrids imported from respective service directory.')
+    grids = SCGrids()
+    print('SCGrids instantiated.')
+
+    read_building_resp = grids.readBuilding(org, pid, test_client)
+
+    print('Read building request complete. Response is:')
+    print(read_building_resp)
+
+    print(f'{test_client} client was used for this request. Response will be parsed accordingly.')
+
+    # region Parse response based on type of client
+    if test_client == CLIENT_TYPE_ASYNC:
+        response_content = read_building_resp
+        print('Obtained response')
+    else:
+        status_code = read_building_resp.status_code
+        print(f'Status code in this response is: {status_code}')
+        response_content = read_building_resp.json()
+        print('Obtained .json() from response.')
+    # endregion
+
+    print('Type of response content is:')
+    type_response = type(response_content)
+    print(type_response)
+
+    if type_response is HTTPClientError:
+        _status_text = 'Error in HTTP Request by sc-python-sdk'
+        _err_text = response_content.message
+        _status_code = response_content.code
+        return_text = f'{_status_text}: code: {_status_code}| message: {_err_text}'
+        print(return_text)
+        return
+
+    print('Response content is:')
+    print(pformat(response_content))
+
+    if EXTRACT_DATA_FROM_SDK_RESPONSE is True:
+        desired_data = response_content['data']
+        print('Extracted "data" from response. Data is:')
+        print(pformat(desired_data))
+
+
+def test_get_zone_info(org: str = None, pid: str = None, zone_id: str = None, test_client=CLIENT_TYPE_ASYNC):
+
+    print('Starting test for Get Zone Info...')
+
+    if org is None:
+        org = os.environ['TEST_ORG']
+
+    if pid is None:
+        pid = os.environ['TEST_PID']
+
+    if zone_id is None:
+        zone_id = os.environ['TEST_ZONE_ID']
+
+    from SDK.SCGridsServices.API import SCGrids
+    print('SCGrids imported from respective service directory.')
+    grids = SCGrids()
+    print('SCGrids instantiated.')
+
+    request_body = {
+        'InsID': zone_id
+    }
+
+    read_zone_resp = grids.read_zone(org, pid, json.dumps(request_body), test_client)
+
+    print('Read zone request complete. Response is:')
+    print(read_zone_resp)
+
+    print(f'{test_client} client was used for this request. Response will be parsed accordingly.')
+
+    # region Parse response based on type of client
+    if test_client == CLIENT_TYPE_ASYNC:
+        response_content = read_zone_resp
+        print('Obtained response')
+    else:
+        status_code = read_zone_resp.status_code
+        print(f'Status code in this response is: {status_code}')
+        response_content = read_zone_resp.json()
+        print('Obtained .json() from response.')
+    # endregion
+
+    print('Type of response content is:')
+    type_response = type(response_content)
+    print(type_response)
+
+    if type_response is HTTPClientError:
+        _status_text = 'Error in HTTP Request by sc-python-sdk'
+        _err_text = response_content.message
+        _status_code = response_content.code
+        return_text = f'{_status_text}: code: {_status_code}| message: {_err_text}'
+        print(return_text)
+        return
+
+    print('Response content is:')
+    print(pformat(response_content))
+
+    if EXTRACT_DATA_FROM_SDK_RESPONSE is True:
+        desired_data = response_content['data']
+        print('Extracted "data" from response. Data is:')
+        print(pformat(desired_data))
+
+
 def load_env_vars(dotenv_filepath: str = None):
 
     print('Loading variables from standard environment file into environment...')
@@ -89,7 +212,7 @@ def run_test():
 
     if LOAD_ENV_VARS is True:
         load_env_vars()
-    test_get_property_info()
+    test_get_zone_info()
 
 
 if __name__ == '__main__':
